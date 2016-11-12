154c0f12c468a8af33562dff12b1bb0e5b659df9
bergi <bergi@axolotlfarm.org>
2016-11-12 23:22:08 +0100

    updated fuseki version and use https URL

--- install.js
+++ install.js
@@ -4,14 +4,14 @@ require('shelljs/global')
 
 var path = require('path')
 
-var fusekiTar = 'apache-jena-fuseki-2.4.0.tar.gz'
+var fusekiTar = 'apache-jena-fuseki-2.4.1.tar.gz'
 var fusekiDownload = path.join(__dirname, 'downloads', fusekiTar)
 var fusekiUnzip = path.join(__dirname, fusekiTar.split('.').slice(0, -2).join('.'))
 var fusekiApp = path.join(__dirname, 'fuseki')
 
 if (!test('-f', fusekiDownload)) {
   mkdir('-p', 'downloads')
-  exec('wget http://mirror.synyx.de/apache/jena/binaries/' + fusekiTar + ' -O "' + fusekiDownload + '"')
+  exec('wget https://mirror.synyx.de/apache/jena/binaries/' + fusekiTar + ' -O "' + fusekiDownload + '"')
 }
 
 if (!test('-d', fusekiApp)) {
